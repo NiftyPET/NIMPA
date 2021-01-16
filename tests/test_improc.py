cae2fe9aafa256925b7126c24759750c77327698
Casper da Costa-Luis <casper.dcl@physics.org>
2021-01-16 19:22:00 +0000

    tests: add improc.convolve

---
+++ tests/test_improc.py
@@ -0,0 +1,28 @@
+import logging
+
+import numpy as np
+from pytest import importorskip
+
+cuvec = importorskip("niftypet.nimpa.cuvec")
+improc = importorskip("niftypet.nimpa.prc.improc")
+
+
+def test_convolve():
+    dst = cuvec.Vector_f((64, 64, 64))
+    src = cuvec.Vector_f((64, 64, 64))
+    knl = cuvec.Vector_f((3, 17))
+
+    dst_arr = np.asarray(dst)
+    src_arr = np.asarray(src)
+    knl_arr = np.asarray(knl)
+    src_arr[32, 32, 32] = 1
+    knl_arr[:, 17//2 - 1] = 0.25
+    knl_arr[:, 17 // 2] = 0.5
+    knl_arr[:, 17//2 + 1] = 0.25
+
+    improc.convolve(dst, src=src, knl=knl, log=logging.DEBUG, memset=False)
+
+    assert dst_arr[31, 31, 31] == 1 / 64
+    assert dst_arr[31, 32, 31] == 1 / 32
+    assert dst_arr[31, 32, 32] == 1 / 16
+    assert dst_arr[32, 32, 32] == 1 / 8
