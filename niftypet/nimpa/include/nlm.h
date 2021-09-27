5906d68b9c5a83dd78b08e741aa6212ae1b27ab7
Casper da Costa-Luis <casper.dcl@physics.org>
2021-09-27 03:18:59 +0100

    misc minor tidy

--- niftypet/nimpa/include/nlm.h
+++ niftypet/nimpa/include/nlm.h
@@ -2,7 +2,7 @@
 #define _NIMPA_NLM_H_
 
 /// main NLM function
-void d_nlm3d(float *dst, const float *src, const float *ref, float sigma, int X, int Y, int Z,
+void d_nlm3d(float *dst, const float *src, const float *ref, float sigma, int Z, int Y, int X,
              int HALF_WIDTH, bool _sync);
 
 #endif // _NIMPA_NLM_H_
