fixed a bug in trimming for higher scaling

niftypet/nimpa/prc/prc.py
@@ -217,8 +217,8 @@ def im_project3(im):
 
 def imtrimup(fims, refim='', affine=None, scale=2, divdim=8**2, fmax=0.05, int_order=0,
              outpath=None, fname='', fcomment='', fcomment_pfx='', store_avg=False,
-             store_img_intrmd=False, store_img=False, imdtype=np.float32, memlim=False,
-             verbose=False, Cnt=None):
+             store_img_intrmd=False, store_img=False, imdtype=np.float32,
+             grid_mode=True, memlim=False, verbose=False, Cnt=None):
     '''
     Trim and upsample PET image(s), e.g., for GPU execution,
     PVC correction, ROI sampling, etc.
@@ -245,6 +245,8 @@ def imtrimup(fims, refim='', affine=None, scale=2, divdim=8**2, fmax=0.05, int_o
     store_img_intrmd: stores intermediate images with suffix '_i'
     store_avg: stores the average image (if multiple images are given)
     imdtype: data type for output images
+    grid_mode: mode for scipy zooming.  Default is True, where the distance including 
+               the full pixel extent is used.
     memlim: Ture for cases when memory is limited and takes more processing time instead.
     verbose: verbose mode [True/False]
     '''
@@ -376,8 +378,9 @@ def imtrimup(fims, refim='', affine=None, scale=2, divdim=8**2, fmax=0.05, int_o
                         disable=log.getEffectiveLevel() > logging.INFO,
                         leave=log.getEffectiveLevel() <= logging.INFO) as pbar:
                 for i in pbar:
-                    imscl[i, :, :, :] = ndi.interpolation.zoom(imin[i, :, :, :], tuple(scale),
-                                                               order=int_order)
+                    imscl[i, :, :, :] = ndi.interpolation.zoom(
+                                            imin[i, :, :, :], tuple(scale),
+                                            order=int_order, grid_mode=grid_mode)
                     imsum += imscl[i, :, :, :]
         else:
             with trange(Nim, desc="loading-scaling",
@@ -386,11 +389,14 @@ def imtrimup(fims, refim='', affine=None, scale=2, divdim=8**2, fmax=0.05, int_o
                 for i in pbar:
                     if Nim > 50 and using_multiple_files:
                         imin_temp = imio.getnii(imdic['files'][i])
-                        imsum += ndi.interpolation.zoom(imin_temp, tuple(scale), order=int_order)
+                        imsum += ndi.interpolation.zoom(
+                                    imin_temp, tuple(scale),
+                                    order=int_order, grid_mode=grid_mode)
                         log.debug(' image sum: read {}'.format(imdic['files'][i]))
                     else:
-                        imsum += ndi.interpolation.zoom(imin[i, :, :, :], tuple(scale),
-                                                        order=int_order)
+                        imsum += ndi.interpolation.zoom(
+                                    imin[i, :, :, :], tuple(scale),
+                                    order=int_order, grid_mode=grid_mode)
     else:
         imscl = imin
         imsum = np.sum(imin, axis=0)
@@ -488,9 +494,9 @@ def imtrimup(fims, refim='', affine=None, scale=2, divdim=8**2, fmax=0.05, int_o
     A = np.diag(np.append(sf[::-1], 1.) * np.diag(affine))
 
     # > note half of new voxel offset is used for the new centre of voxels
-    A[0, 3] = affine[0, 3] + A[0, 0] * (ix0-0.5)
-    A[1, 3] = affine[1, 3] + (affine[1, 1] * (imshape[1] - 1) - A[1, 1] * (iy1-0.5))
-    A[2, 3] = affine[2, 3] - A[1, 1] * 0.5
+    A[0, 3] = affine[0, 3] + A[0, 0] * (ix0-0.5*(scale[2]-1))
+    A[1, 3] = affine[1, 3] + (affine[1, 1] * (imshape[1] - 1) - A[1, 1] * (iy1-0.5*(scale[1]-1)))
+    A[2, 3] = affine[2, 3] - A[2, 2] * 0.5*(scale[0]-1)
     A[3, 3] = 1
 
     # output dictionary