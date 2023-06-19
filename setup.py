7f6a14782d045d27a5b12bbd9009acacfdc7ddd3
Casper da Costa-Luis <casper.dcl@physics.org>
2023-06-19 22:55:03 +0100

    build: fix dipy install error

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup_kwargs = {
         "niftypet": [
             "nimpa/auxdata/*", "nimpa/acr_design/core_mumap/*", "nimpa/acr_design/core_nac/*",
             "nimpa/acr_design/rods/*", "nimpa/acr_design/sampling/*"]}, "install_requires": [
-                'dipy>=1.3.0', 'miutil[nii]>=0.10.0', 'nibabel>=2.4.0', 'ninst>=0.12.0',
-                'numpy>=1.14', 'pydicom>=1.0.2', 'scipy', 'spm12', 'imageio>=2.22.4', 'dcm2niix',
-                'importlib_resources; python_version < "3.9"', 'SimpleITK']}
+                'dcm2niix', 'dipy>=1.3.0', 'imageio>=2.22.4', 'miutil[nii]>=0.10.0',
+                'nibabel>=2.4.0', 'ninst>=0.12.0', 'numpy>=1.14', 'pydicom>=1.0.2', 'scipy',
+                'spm12', 'importlib_resources; python_version < "3.9"', 'SimpleITK']}
 # 'SimpleITK>=1.2.0'
 cmake_args = [
     f"-DNIMPA_BUILD_VERSION={build_ver}",
